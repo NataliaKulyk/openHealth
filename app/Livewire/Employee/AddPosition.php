4f0f1faeb8dd4715109f572f52449aa0d6716d48
Andrii Kopylets <andr.kopylets@gmail.com>
2025-06-25 16:01:05 +0300

    i238_new_route_add_component

--- app/Livewire/Employee/AddPosition.php
+++ app/Livewire/Employee/AddPosition.php
@@ -5,27 +5,30 @@ namespace App\Livewire\Employee;
 use App\Livewire\Employee\Forms\EmployeeForm;
 use App\Livewire\Employee\Traits\ManagesEmployeeForm;
 use App\Models\LegalEntity;
+use App\Models\Relations\Party;
 use Illuminate\View\View;
 
-class EmployeeCreate extends EmployeeComponent
+class AddPosition extends EmployeeComponent
 {
     use ManagesEmployeeForm;
 
     public EmployeeForm $form;
-    public string $pageTitle;
+    public string $pageTitle = 'Додавання нової посади';
 
-    public function mount(LegalEntity $legalEntity): void
+    public function mount(LegalEntity $legalEntity, Party $party): void
     {
         $this->getDictionary();
-        $this->lockPartyFields = false;
-        $this->pageTitle = __('forms.add_position');
+
+        $this->form->populateFromParty($party);
+
+        $this->lockPartyFields = true;
     }
 
     public function render(): View
     {
+        // Now points to the new unified view
         return view('livewire.employee.employee', [
             'pageTitle' => $this->pageTitle,
-            'employee' => $this->employee,
         ]);
     }
 }
