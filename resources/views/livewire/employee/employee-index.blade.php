12ad987d35e8a92cad2c289c8d8ecc5e368ffbc9
Andrii Kopylets <andr.kopylets@gmail.com>
2025-06-25 19:22:02 +0300

    i238_sign_refactor

--- resources/views/livewire/employee/employee-index.blade.php
+++ resources/views/livewire/employee/employee-index.blade.php
@@ -110,9 +110,6 @@
                             <a href="{{ route('employee.add-position', ['legalEntity' => legalEntity()->id, 'party' => $party->id]) }}" class="button-secondary-outline">
                                 {{ __('forms.addPosition') }}
                             </a>
-                            <a href="{{ route('employee.edit', ['legalEntity' => legalEntity()->id, 'employeeId' => $party->employees->first()?->id ?? 0]) }}" class="text-gray-500 hover:text-blue-600 p-2">
-                                <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"></path></svg>
-                            </a>
                         </div>
                     </div>
 
