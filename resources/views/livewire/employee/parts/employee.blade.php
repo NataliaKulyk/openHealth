e8e143fc6604c5d55e36b29099cce0a1d7e21389
Natalia <nataliakulyk123@gmail.com>
2025-06-26 14:54:52 +0300

    saving local changes

--- resources/views/livewire/employee/parts/employee.blade.php
+++ resources/views/livewire/employee/parts/employee.blade.php
@@ -5,33 +5,30 @@
     <form class="form">
         <div class="form-row-3">
             <div class="form-group">
-                <input type="text" name="lastName" id="lastName" class="peer input text-gray-500" placeholder=" " required />
+                <input wire:model="form.party.lastName" type="text" name="lastName" id="lastName" class="peer input text-gray-500" placeholder=" " required />
                 <label for="lastName" class="label">{{__('forms.last_name')}}</label>
                 @error('form.party.lastName') <p class="text-error">{{$message}}</p> @enderror
             </div>
             <div class="form-group">
-                <input type="text" name="firstName" id="firstName" class="peer input text-gray-500" placeholder=" " required />
+                <input wire:model="form.party.firstName"  type="text" name="firstName" id="firstName" class="peer input text-gray-500" placeholder=" " required />
                 <label for="firstName" class="label">{{__('forms.first_name')}}</label>
                 @error('form.party.firstName') <p class="text-error">{{$message}}</p> @enderror
             </div>
         </div>
         <div class="form-row-3">
             <div class="form-group">
-                <input type="text" name="secondName" id="secondName" class="peer input text-gray-500" placeholder=" " />
+                <input wire:model="form.party.secondName" type="text" name="secondName" id="secondName" class="peer input text-gray-500" placeholder=" " />
                 <label for="secondName" class="label">{{ __('forms.second_name') }}</label>
                 @error('form.party.secondName') <p class="text-error">{{$message}}</p> @enderror
             </div>
             <div class="form-group">
-                <select name="employeeGender" id="employeeGender" class="peer input appearance-none bg-white text-gray-500 dark:bg-gray-800 dark:text-gray-400" required>
-                    <option value="" disabled selected hidden>Оберіть стать</option>
+                <select wire:model="form.party.gender" name="employeeGender" id="employeeGender" class="peer input appearance-none bg-white text-gray-500 dark:bg-gray-800 dark:text-gray-400" required>
+                    <option value="" disabled selected hidden>{{ __('forms.select') }} {{ __('forms.gender') }}</option>
                     @foreach($this->dictionaries['GENDER'] as $k => $gender)
                         <option value="{{ $k }}">{{ $gender }}</option>
                     @endforeach
                 </select>
-                <label for="employeeGender"
-                       class="label">
-                    Стать
-                </label>
+                <label for="employeeGender" class="label">{{ __('forms.gender') }}</label>
                 @error('form.party.gender')
                 <p class="text-error">{{ $message }}</p>
                 @enderror
@@ -39,12 +36,12 @@
         </div>
         <div class="form-row-3">
             <div class="form-group datepicker-wrapper relative w-full">
-                <input type="text" name="birthDate" id="birthDate" class="peer input pl-10 appearance-none datepicker-input text-gray-500 dark:text-gray-400" placeholder=" " required datepicker-autohide datepicker-format="yyyy-mm-dd" datepicker-button="false"/>
+                <input wire:model="form.party.birthDate" type="text" name="birthDate" id="birthDate" class="peer input pl-10 appearance-none datepicker-input text-gray-500 dark:text-gray-400" placeholder=" " required datepicker-autohide datepicker-format="yyyy-mm-dd" datepicker-button="false"/>
                 <label for="birthDate" class="wrapped-label">{{__('forms.birth_date')}}</label>
                 @error('form.party.birthDate') <p class="text-error">{{$message}}</p> @enderror
             </div>
             <div class="form-group">
-                <input type="number" name="workingExperience" id="workingExperience" class="peer input text-gray-500" placeholder=" " required />
+                <input wire:model="form.party.workingExperience" type="number" name="workingExperience" id="workingExperience" class="peer input text-gray-500" placeholder=" " required />
                 <label for="workingExperience" class="label">{{__('forms.workingExperience')}}</label>
                 @error('form.party.workingExperience') <p class="text-error">{{$message}}</p> @enderror
             </div>
@@ -54,7 +51,7 @@
             x-data="{ showNoTaxId: $wire.entangle('form.party.noTaxId') }"
         >
             <div class="form-group group relative z-0">
-                <input required id="taxId" type="text" name="taxId" maxlength="10" placeholder=" " wire:model="form.party.taxId" aria-describedby="{{ $errors->has('form.party.taxId') ? 'partyTaxIdErrorHelp' : '' }}" class="input {{ $errors->has('form.party.taxId') ? 'input-error border-red-500 focus:border-red-500' : ''}} peer text-gray-500"
+                <input wire:model="form.party.taxId" required id="taxId" type="text" name="taxId" maxlength="10" placeholder=" " wire:model="form.party.taxId" aria-describedby="{{ $errors->has('form.party.taxId') ? 'partyTaxIdErrorHelp' : '' }}" class="input {{ $errors->has('form.party.taxId') ? 'input-error border-red-500 focus:border-red-500' : ''}} peer text-gray-500"
                 />
                 @error('form.party.taxId')
                 <p id="partyTaxIdErrorHelp" class="text-error">
@@ -80,15 +77,13 @@
         </div>
         <div class="form-row-3">
             <div class="form-group">
-                <select name="phoneType" id="phoneType-@{{ index }}" class="peer input appearance-none bg-white text-gray-500 dark:bg-gray-800 dark:text-gray-400" required>
-                    <option value="" disabled selected hidden>Оберіть тип номера</option>
+                <select x-model = "phone.type" name="phoneType" id="phoneType-@{{ index }}" class="peer input appearance-none bg-white text-gray-500 dark:bg-gray-800 dark:text-gray-400" required>
+                    <option value="" disabled selected hidden>{{ __('forms.type_mobile') }}</option>
                     @foreach($this->dictionaries['PHONE_TYPE'] as $k => $phoneType)
                         <option value="{{ $k }}">{{ $phoneType }}</option>
                     @endforeach
                 </select>
-                <label for="phoneType-@{{ index }}" class="label">
-                    Тип номера
-                </label>
+                <label for="phoneType" class="label">{{ __('forms.phone_type') }}</label>
                 <p class="text-error"
                    x-text="$wire.errors.get('form.party.phones.' + index + '.type')"
                    x-show="$wire.errors.has('form.party.phones.' + index + '.type')">
@@ -112,7 +107,7 @@
         </div>
         <div class="form-row-3">
             <div class="form-group">
-                <input type="text" name="email" id="email" class="peer input text-gray-500" placeholder=" " required />
+                <input wire:model="form.party.email" type="text" name="email" id="email" class="peer input text-gray-500" placeholder=" " required />
                 <label for="email" class="label">{{__('forms.email')}}</label>
                 @error('form.party.email') <p class="text-error">{{$message}}</p> @enderror
             </div>
