8954842fb966368b7bee603bf2ad477173a4c978
KarbovskyiV <karbovskyi.volodymyr@gmail.com>
2025-07-01 11:32:51 +0300

    Fixes #244, also add preprod scopes for DOCTOR

--- config/ehealth.php
+++ config/ehealth.php
@@ -600,6 +600,14 @@ return [
             'device_dispense:read',
             'device_association:read',
             'detected_issue:read',
+            // Only preprod
+            'composition:write',
+            'composition:mark_in_error',
+            'composition:resend_sms',
+            'composition_configuration:read',
+            'person_verification:read',
+            'confidant_person_relationship_request:write',
+            'confidant_person_relationship:read'
         ],
         'HR' => [
             'division:activate',
