10986adddd7f70c110b5a120dc1f4a7ae2904511
Natalia <nataliakulyk123@gmail.com>
2025-07-04 15:16:48 +0300

    fix list

--- resources/views/livewire/employee/employee-index.blade.php
+++ resources/views/livewire/employee/employee-index.blade.php
@@ -1,3 +1,4 @@
+<body class="bg-white dark:bg-gray-800 text-gray-600 dark:text-gray-400">
 <div>
     <x-section-navigation x-data="{ showFilter: false }">
 
@@ -7,29 +8,42 @@
 
         <x-slot name="navigation">
             <div class="flex flex-col">
-                <div class="flex flex-wrap items-start justify-between gap-4">
-                    <div class="flex-1 min-w-0">
-                        <x-forms.form-group>
-                            <x-slot name="label">
-                                <label for="employee_search" class="text-sm font-medium text-gray-900 dark:text-white">
-                                    <svg class="w-4 h-4 text-gray-500 dark:text-gray-400 inline-block mr-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
-                                    </svg>
-                                    {{ __('forms.employeeSearch') }}
-                                </label>
-                            </x-slot>
-                            <x-slot name="input">
-                                <x-forms.input class="default-input" wire:model.live.debounce.300ms="search"
-                                               type="text" id="employee_search" placeholder="{{ __('forms.full_name') }}" autocomplete="off" />
-                            </x-slot>
-                        </x-forms.form-group>
+                <div class="flex flex-wrap items-end justify-between gap-4"> <div class="flex items-end gap-4"> <div class="w-96">
+                            <x-forms.form-group>
+                                <x-slot name="label">
+                                    <label for="employee_search" class="text-sm font-medium text-gray-900 dark:text-white block mb-2 flex items-center gap-1">
+                                        <svg class="w-4 h-4 text-gray-500 dark:text-gray-400"
+                                             aria-hidden="true"
+                                             xmlns="http://www.w3.org/2000/svg"
+                                             fill="none"
+                                             viewBox="0 0 20 20"
+                                        >
+                                            <path stroke="currentColor"
+                                                  stroke-linecap="round"
+                                                  stroke-linejoin="round"
+                                                  stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+                                        </svg>
+                                        <span>{{ __('forms.employeeSearch') }}</span>
+                                    </label>
+                                </x-slot>
+                                <x-slot name="input">
+                                    <x-forms.input class="default-input" wire:model.live.debounce.300ms="search"
+                                                   type="text" id="employee_search" placeholder="{{ __('forms.full_name') }}" autocomplete="off" />
+                                </x-slot>
+                            </x-forms.form-group>
+                        </div>
+
+                        <button class="flex items-center gap-2 gray-button relative top-[8px]" @click="showFilter = !showFilter">
+                            <svg width="16" height="16" id="svg-adjustments" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path
+                                    d="M4.00003 3.1999C4.00003 2.98773 3.91574 2.78425 3.76571 2.63422C3.61568 2.48419 3.4122 2.3999 3.20003 2.3999C2.98785 2.3999 2.78437 2.48419 2.63434 2.63422C2.48431 2.78425 2.40003 2.98773 2.40003 3.1999V9.0143C2.15682 9.15474 1.95485 9.35672 1.81444 9.59994C1.67402 9.84316 1.6001 10.1191 1.6001 10.3999C1.6001 10.6807 1.67402 10.9566 1.81444 11.1999C1.95485 11.4431 2.15682 11.6451 2.40003 11.7855V12.7999C2.40003 13.0121 2.48431 13.2156 2.63434 13.3656C2.78437 13.5156 2.98785 13.5999 3.20003 13.5999C3.4122 13.5999 3.61568 13.5156 3.76571 13.3656C3.91574 13.2156 4.00003 13.0121 4.00003 12.7999V11.7855C4.24324 11.6451 4.4452 11.4431 4.58562 11.1999C4.72603 10.9566 4.79996 10.6807 4.79996 10.3999C4.79996 10.1191 4.72603 9.84316 4.58562 9.59994C4.4452 9.35672 4.24324 9.15474 4.00003 9.0143V3.1999ZM8.80003 3.1999C8.80003 2.98773 8.71574 2.78425 8.56571 2.63422C8.41568 2.48419 8.2122 2.3999 8.00003 2.3999C7.78785 2.3999 7.58437 2.48419 7.43434 2.63422C7.28431 2.78425 7.20003 2.98773 7.20003 3.1999V4.2143C6.95682 4.35474 6.75485 4.55672 6.61444 4.79994C6.47402 5.04316 6.4001 5.31906 6.4001 5.5999C6.4001 5.88075 6.47402 6.15665 6.61444 6.39987C6.75485 6.64309 6.95682 6.84507 7.20003 6.9855V12.7999C7.20003 13.0121 7.28431 13.2156 7.43434 13.3656C7.58437 13.5156 7.78785 13.5999 8.00003 13.5999C8.2122 13.5999 8.41568 13.5156 8.56571 13.3656C8.71574 13.2156 8.80003 13.0121 8.80003 12.7999V6.9855C9.04324 6.84507 9.2452 6.64309 9.38562 6.39987C9.52603 6.15665 9.59996 5.88075 9.59996 5.5999C9.59996 5.31906 9.52603 5.04316 9.38562 4.79994C9.2452 4.55672 9.04324 4.35474 8.80003 4.2143V3.1999ZM12.8 2.3999C13.0122 2.3999 13.2157 2.48419 13.3657 2.63422C13.5157 2.78425 13.6 2.98773 13.6 3.1999V9.0143C13.8432 9.15474 14.0452 9.35672 14.1856 9.59994C14.326 9.84316 14.4 10.1191 14.4 10.3999C14.4 10.6807 14.326 10.9566 14.1856 11.1999C14.0452 11.4431 13.8432 11.6451 13.6 11.7855V12.7999C13.6 13.0121 13.5157 13.2156 13.3657 13.3656C13.2157 13.5156 13.0122 13.5999 12.8 13.5999C12.5879 13.5999 12.3844 13.5156 12.2343 13.3656C12.0843 13.2156 12 13.0121 12 12.7999V11.7855C11.7568 11.6451 11.5549 11.4431 11.4144 11.1999C11.274 10.9566 11.2001 10.6807 11.2001 10.3999C11.2001 10.1191 11.274 9.84316 11.4144 9.59994C11.5549 9.35672 11.7568 9.15474 12 9.0143V3.1999C12 2.98773 12.0843 2.78425 12.2343 2.63422C12.3844 2.48419 12.5879 2.3999 12.8 2.3999V2.3999Z"
+                                    fill="currentColor"/>
+                            </svg>
+                            <span>{{ __('patients.additional_search_parameters') }}</span>
+                        </button>
                     </div>
 
                     <div class="flex items-center space-x-2 pt-5">
-                        <button type="button" @click="showFilter = !showFilter" class="button-secondary-outline">
-                            <svg class="w-5 h-5 mr-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7.75 4H19M7.75 4a2.25 2.25 0 0 1-4.5 0m4.5 0a2.25 2.25 0 0 0-4.5 0M1 4h2.25m15.75 8H1M1 12h10.25m6.75 0a2.25 2.25 0 0 1-4.5 0m4.5 0a2.25 2.25 0 0 0-4.5 0M1 12h.01M19 12h.01M1 20h2.25m15.75 0H1M1 20h10.25m6.75 0a2.25 2.25 0 0 1-4.5 0m4.5 0a2.25 2.25 0 0 0-4.5 0M1 20h.01M19 20h.01"/></svg>
-                            Додаткові параметри
-                        </button>
                         <a href="{{ route('employee.create', ['legalEntity' => legalEntity()->id]) }}" class="button-primary">
                             {{ __('forms.newEmployee') }}
                         </a>
@@ -130,16 +144,18 @@
                                 @endif
                                 @if($party->email)
                                     <span class="flex items-center gap-1.5">
-                                        <svg class="w-4 h-4" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20"><path d="M2.003 5.884L10 2.25l7.997 3.634A1 1 0 0017 4.584V15.5a1 1 0 00-1.997-.084L10 11.75l-5.003 3.666A1 1 0 003 15.5V4.584a1 1 0 00-1.997 1.25l.004-.001z"></path></svg>
-                                        <a href="mailto:{{$party->email}}" class="hover:underline">{{ $party->email }}</a>
+                                        <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
+                <path d="M2.038 5.61A2.01 2.01 0 0 0 2 6v12a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V6c0-.12-.01-.238-.03-.352l-.866.65-7.89 6.032a2 2 0 0 1-2.429 0L2.884 6.288l-.846-.677Z"/>
+                <path d="M20.677 4.117A1.996 1.996 0 0 0 20 4H4c-.225 0-.44.037-.642.105l.758.607L12 10.742 19.9 4.7l.777-.583Z"/>
+            </svg><a href="mailto:{{$party->email}}" class="hover:underline">{{ $party->email }}</a>
                                     </span>
                                 @endif
                             </div>
                         </div>
                         <div class="flex items-center space-x-3">
-                            <a href="{{ route('employee.add-position', ['legalEntity' => legalEntity()->id, 'party' => $party->id]) }}" class="button-secondary-outline">
-                                {{ __('forms.addPosition') }}
-                            </a>
+                            <button type="button" wire:click="addPhone" class="item-add">
+                                <span>{{__('forms.addPosition')}}</span>
+                            </button>
                         </div>
                     </div>
 
@@ -157,8 +173,8 @@
                             <tbody>
                             @php $positions = $party->employees->merge($party->employeeRequests); @endphp
                             @foreach($positions as $position)
-                                <tr class="border-b dark:border-gray-700">
-                                    <td class="px-4 py-3 font-medium">{{ $dictionaries['POSITION'][$position->position] ?? $position->position }}</td>
+                                <tr class="border-b-4 border-gray-200 dark:border-gray-700">
+                                <td class="px-4 py-3 font-medium">{{ $dictionaries['POSITION'][$position->position] ?? $position->position }}</td>
                                     <td class="px-4 py-3">{{ $dictionaries['EMPLOYEE_TYPE'][$position->employee_type] ?? $position->employee_type }}</td>
                                     <td class="px-4 py-3">{{ $position->division->name ?? 'N/A' }}</td>
 
@@ -265,3 +281,4 @@
         </template>
     </div>
 </div>
+</body>
