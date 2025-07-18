139b9dbcf779d1da0add1718f6abbf814d75dd5d
Natalia <nataliakulyk123@gmail.com>
2025-07-18 11:35:50 +0300

    #260 fix problem

--- resources/views/livewire/license/license-view.blade.php
+++ resources/views/livewire/license/license-view.blade.php
@@ -1,66 +1,58 @@
 <div>
-    <x-section-navigation x-data="{ showFilter: false }" class=''>
-        <x-slot name='title'>{{ __('Деталі Ліцензії') }}</x-slot>
-        <x-slot name='description'>{{ __('Деталі Ліцензії') }}</x-slot>
+    <x-section-navigation class="breadcrumb-form">
+        <x-slot name="title">{{ __('Деталі ліцензії') }}</x-slot>
     </x-section-navigation>
-
-    <div class='inline-block min-w-full align-middle p-6 bg-white'>
-        <div class='w-full p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700'>
-            <div class='items-center border-stroke px-7 py-4 dark:border-strokedark'>
-                <div class='mb-4'>
-                    <p>
-                        <strong>{{ __('forms.type') }}:</strong> {{ $licenseTypes[$license->type] }}
-                    </p>
-                </div>
-                <div class='mb-4'>
-                    <p>
-                        <strong>{{ __('Ким видано ліцензію') }}:</strong> {{ $license->issued_by }}
-                    </p>
-                </div>
-                <div class='mb-4'>
-                    <p>
-                        <strong>{{ __('Дата видачі ліцензії') }}:</strong> {{ $license->issued_date }}
-                    </p>
-                </div>
-                <div class='mb-4'>
-                    <p>
-                        <strong>{{ __('Дата початку дії ліцензії') }}:</strong> {{ $license->active_from_date }}
-                    </p>
-                </div>
-                <div class='mb-4'>
-                    <p>
-                        <strong>{{ __('Номер наказу') }}:</strong> {{ $license->order_no }}
-                    </p>
-                </div>
-                <div class='mb-4'>
-                    <p>
-                        <strong>{{ __('Cерія та/або номер ліцензії') }}:</strong> {{ $license->license_number }}
-                    </p>
-                </div>
-                @if ($license->expiry_date)
-                    <div class='mb-4'>
-                        <p>
-                            <strong>{{ __('Дата завершення дії ліцензії') }}:</strong> {{ $license->expiry_date }}
-                        </p>
-                    </div>
-                @endif
-                <div class='mb-4'>
-                    <p>
-                        <strong>{{ __('Напрям діяльності, що ліцензовано') }}:</strong> {{ $license->what_licensed }}
-                    </p>
-                </div>
-                <div class='mb-4'>
-                    <p>
-                        <strong>{{ __('Основна ліцензія') }}:</strong> {{ $license->is_primary === 1 ? __('forms.yes') : __('forms.no') }}
-                    </p>
-                </div>
+    <form class="form">
+        <div class="form-row-2">
+            <div class="form-group">
+                <input type="text" name="licenseKind" id="licenseKind" class="peer input dark:text-gray-400" value="Основна" placeholder=" " required />
+                <label for="licenseKind" class="label">Вид ліцензії</label>
+            </div>
+            <div class="form-group">
+                <input type="text" name="OrderNumber" id="OrderNumber" class="peer input dark:text-gray-400" value="123123" placeholder=" " required />
+                <label for="OrderNumber" class="label">Номер наказу</label>
             </div>
         </div>
-    </div>
-
-    <div class='flex flex-start border-stroke px-7 py-2 mt-4'>
-        <x-secondary-button wire:click='back'>
-            {{ __('Назад') }}
-        </x-secondary-button>
-    </div>
+        <div class="form-row">
+            <div class="form-group">
+                <input type="text" name="licenseSeriesNumber" id="licenseSeriesNumber" class="peer input dark:text-gray-400" value="Провадження господарської діяльності з медичної практики" placeholder=" " required />
+                <label for="LicenseSeriesNumber" class="label">Тип ліцензії</label>
+            </div>
+        </div>
+        <div class="form-row-2">
+            <div class="form-group">
+                <input type="text" name="IssuedTheLicense" id="IssuedTheLicense" class="peer input dark:text-gray-400" value="МОЗ" placeholder=" " required />
+                <label for="IssuedTheLicense" class="label">Ким видано</label>
+            </div>
+            <div class="form-group">
+                <input type="text" name="licensedActivity" id="licensedActivity" class="peer input dark:text-gray-400" value="Лікарська діяльність" placeholder=" " required />
+                <label for="licensedActivity" class="label">Напрям діяльності, що ліцензовано</label>
+            </div>
+        </div>
+        <div class="form-row-2">
+            <div class="form-group">
+                <input type="text" name="licenseSeriesNumber" id="licenseSeriesNumber" class="peer input dark:text-gray-400" value="1231" placeholder=" " required />
+                <label for="LicenseSeriesNumber" class="label">Серія та/або номер ліцензії</label>
+            </div>
+            <div class="form-group datepicker-wrapper relative w-full">
+                <input type="text" name="dateOfLicenseIssuance" id="dateOfLicenseIssuance" class="peer input pl-10 appearance-none datepicker-input dark:text-gray-400" value="2025-02-02" placeholder=" " required datepicker-autohide datepicker-format="yyyy-mm-dd" datepicker-button="false"/>
+                <label for="dateOfLicenseIssuance" class="wrapped-label">Дата видачі ліцензії</label>
+            </div>
+        </div>
+        <div class="form-row-2">
+            <div class="form-group datepicker-wrapper relative w-full">
+                <input type="text" name="dateOfLicenseStartDate" id="dateOfLicenseStartDate" class="peer input pl-10 appearance-none datepicker-input dark:text-gray-400" value="2025-02-02" placeholder=" " required datepicker-autohide datepicker-format="yyyy-mm-dd" datepicker-button="false"/>
+                <label for="dateOfLicenseStartDate" class="wrapped-label">Дата початку дії ліцензії</label>
+            </div>
+            <div class="form-group datepicker-wrapper relative w-full">
+                <input type="text" name="dateOfLicenseExpiry" id="dateOfLicenseExpiry" class="peer input pl-10 appearance-none datepicker-input dark:text-gray-400" value="2026-02-02" placeholder=" " required datepicker-autohide datepicker-format="yyyy-mm-dd" datepicker-button="false"/>
+                <label for="dateOfLicenseExpiry" class="wrapped-label">Дата завершення дії ліцензії</label>
+            </div>
+        </div>
+        <div class="flex justify-start gap-4 mt-10">
+            <button type="button" class="button-minor">
+                Скасувати
+            </button>
+        </div>
+    </form>
 </div>
