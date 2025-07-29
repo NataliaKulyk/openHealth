f0da0675da73067d7f3a677dc16fae2628b0221f
Natalia <nataliakulyk123@gmail.com>
2025-07-29 17:46:41 +0300

    #286_update_list

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -46,9 +46,6 @@
 
             @stack('modals')
             @stack('scripts')
-
-            @livewire('components.flash-message')
-
             @vite(['resources/js/index.js', 'resources/js/app.js'])
 
             @yield('scripts')
