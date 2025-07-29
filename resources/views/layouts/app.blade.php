135514d6c4a9344b7750a47545a1f009fde5b333
Andrii Kopylets <andr.kopylets@gmail.com>
2025-07-29 12:00:34 +0300

    #253_removing_flash_message

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -47,8 +47,6 @@
             @stack('modals')
             @stack('scripts')
 
-            @livewire('components.flash-message')
-
             @vite(['resources/js/index.js', 'resources/js/app.js'])
 
             @yield('scripts')
