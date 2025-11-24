8259656b366f66f7bf671245497aa95261e99a8f
KarbovskyiV <karbovskyi.volodymyr@gmail.com>
2025-11-24 15:53:13 +0200

    #fix first run

--- database/migrations/2025_10_05_112636_create_declarations_table.php
+++ database/migrations/2025_10_05_112636_create_declarations_table.php
@@ -2,6 +2,7 @@
 
 declare(strict_types=1);
 
+use App\Enums\Declaration\Status;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -29,7 +30,7 @@ return new class extends Migration
             $table->string('reason_description')->nullable();
             $table->dateTime('signed_at');
             $table->date('start_date');
-            $table->string('status');
+            $table->enum('status', Status::values());
             $table->timestamps();
         });
     }
