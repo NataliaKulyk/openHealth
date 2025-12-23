282399b7c40287d77446358c17d4936876589cd7
Andrii Kopylets <andr.kopylets@gmail.com>
2025-12-23 16:53:09 +0200

    #636_revert_migration

--- resources/views/livewire/employee/employee-index.blade.php
+++ resources/views/livewire/employee/employee-index.blade.php
@@ -6,12 +6,12 @@
 
         // Cache access rights with an array
        $permissions = [
-        'employee_view'       => $currentUser->can('employee:details'),
-        'employee_write'      => $currentUser->can('employee:write'),
+        'employee_view' => $currentUser->can('employee:details'),
+        'employee_write' => $currentUser->can('employee:write'),
         'employee_deactivate' => $currentUser->can('employee:deactivate'),
-        'request_view'        => $currentUser->can('employee_request:details'),
-        'request_write'       => $currentUser->can('employee_request:write'),
-        'request_delete'      => $currentUser->can('employee_request:write'),
+        'request_view' => $currentUser->can('employee_request:details'),
+        'request_write' => $currentUser->can('employee_request:write'),
+        'request_delete' => $currentUser->can('employee_request:write'),
     ];
     @endphp
 
@@ -44,8 +44,10 @@
                                                class="text-sm font-medium text-gray-900 dark:text-white block mb-2 flex items-center gap-1">
                                             <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true"
                                                  xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
-                                                      stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+                                                <path stroke="currentColor" stroke-linecap="round"
+                                                      stroke-linejoin="round"
+                                                      stroke-width="2"
+                                                      d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
                                             </svg>
                                             <span>{{ __('forms.employee_search') }}</span>
                                         </label>
@@ -57,7 +59,7 @@
                                                    placeholder=" "
                                                    class="input peer"
                                                    wire:model.defer="search"
-                                                   autocomplete="off" />
+                                                   autocomplete="off"/>
                                             <label for="employee_search" class="label">ПІБ</label>
                                         </div>
                                     </x-slot>
@@ -78,11 +80,13 @@
                                            wire:keydown.enter="applyFilters"
                                            type="tel" placeholder=" "
                                            class="peer input pl-10 with-leading-icon text-gray-500"
-                                           x-mask="+380999999999" id="filter_phone" />
+                                           x-mask="+380999999999" id="filter_phone"/>
                                     <label for="filter_phone" class="label pl-10">{{ __('forms.phone') }}</label>
                                 </div>
                                 <div class="form-group group">
-                                    <input wire:model.defer="filter.email" wire:keydown.enter="applyFilters" name="filter_email" id="filter_email" class="input peer" placeholder=" " autocomplete="off" />
+                                    <input wire:model.defer="filter.email" wire:keydown.enter="applyFilters"
+                                           name="filter_email" id="filter_email" class="input peer" placeholder=" "
+                                           autocomplete="off"/>
                                     <label for="filter_email" class="label">Email</label>
                                 </div>
                             </div>
@@ -126,7 +130,8 @@
                                     </select>
                                     <label for="filter_division" class="label">Медичний заклад</label>
                                 </div>
-                                <div class="form-group group" x-data="{ open: false, selectedStatuses: $wire.entangle('status') }">
+                                <div class="form-group group"
+                                     x-data="{ open: false, selectedStatuses: $wire.entangle('status') }">
                                     <label for="statusFilter" class="label">{{ __('forms.status.label') }}</label>
                                     <div class="relative">
 
@@ -148,7 +153,9 @@
                                                }).join(', ') : ''"
                                                readonly
                                         />
-                                        <svg class="w-4 h-4 absolute right-3 top-1/2 -translate-y-1/2 text-gray-500 dark:text-gray-400 pointer-events-none" fill="none" stroke="currentColor" stroke-width="2" viewBox="0 0 24 24">
+                                        <svg
+                                            class="w-4 h-4 absolute right-3 top-1/2 -translate-y-1/2 text-gray-500 dark:text-gray-400 pointer-events-none"
+                                            fill="none" stroke="currentColor" stroke-width="2" viewBox="0 0 24 24">
                                             <path d="M19 9l-7 7-7-7"></path>
                                         </svg>
                                         <div x-show="open"
@@ -164,14 +171,14 @@
                                                 <li>
                                                     <label class="flex items-center space-x-2 cursor-pointer">
                                                         <input type="checkbox" value="APPROVED" wire:model="status"
-                                                               class="rounded-sm text-blue-600 focus:ring-blue-500 border-gray-300 dark:bg-gray-800 dark:border-gray-600 dark:checked:bg-blue-600 dark:checked:border-transparent" />
+                                                               class="rounded-sm text-blue-600 focus:ring-blue-500 border-gray-300 dark:bg-gray-800 dark:border-gray-600 dark:checked:bg-blue-600 dark:checked:border-transparent"/>
                                                         <span>{{ __('forms.active') }}</span>
                                                     </label>
                                                 </li>
                                                 <li>
                                                     <label class="flex items-center space-x-2 cursor-pointer">
                                                         <input type="checkbox" value="NEW" wire:model="status"
-                                                               class="rounded-sm text-blue-600 focus:ring-blue-500 border-gray-300 dark:bg-gray-800 dark:border-gray-600 dark:checked:bg-blue-600 dark:checked:border-transparent" />
+                                                               class="rounded-sm text-blue-600 focus:ring-blue-500 border-gray-300 dark:bg-gray-800 dark:border-gray-600 dark:checked:bg-blue-600 dark:checked:border-transparent"/>
                                                         <span>{{ __('forms.draft') }}</span>
                                                     </label>
                                                 </li>
@@ -179,7 +186,7 @@
                                                 <li>
                                                     <label class="flex items-center space-x-2 cursor-pointer">
                                                         <input type="checkbox" value="SIGNED" wire:model="status"
-                                                               class="rounded-sm text-blue-600 focus:ring-blue-500 border-gray-300 dark:bg-gray-800 dark:border-gray-600 dark:checked:bg-blue-600 dark:checked:border-transparent" />
+                                                               class="rounded-sm text-blue-600 focus:ring-blue-500 border-gray-300 dark:bg-gray-800 dark:border-gray-600 dark:checked:bg-blue-600 dark:checked:border-transparent"/>
                                                         <span>{{ __('forms.status.sent') }}</span>
                                                     </label>
                                                 </li>
@@ -187,7 +194,7 @@
                                                 <li>
                                                     <label class="flex items-center space-x-2 cursor-pointer">
                                                         <input type="checkbox" value="DISMISSED" wire:model="status"
-                                                               class="rounded-sm text-blue-600 focus:ring-blue-500 border-gray-300 dark:bg-gray-800 dark:border-gray-600 dark:checked:bg-blue-600 dark:checked:border-transparent" />
+                                                               class="rounded-sm text-blue-600 focus:ring-blue-500 border-gray-300 dark:bg-gray-800 dark:border-gray-600 dark:checked:bg-blue-600 dark:checked:border-transparent"/>
                                                         <span>{{ __('forms.dismissed') }}</span>
                                                     </label>
                                                 </li>
@@ -255,10 +262,13 @@
                         });
                     @endphp
 
-                    <fieldset class="p-4 sm:p-8 sm:pb-10 mb-16 mt-6 border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 max-w-[1280px]" wire:key="party-{{ $party->id }}">
+                    <fieldset
+                        class="p-4 sm:p-8 sm:pb-10 mb-16 mt-6 border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 max-w-[1280px]"
+                        wire:key="party-{{ $party->id }}">
                         <legend class="legend">{{ $party->fullName }}</legend>
 
-                        <div class="flex flex-wrap items-center justify-between gap-4 border-b border-gray-200 dark:border-gray-700 pb-4">
+                        <div
+                            class="flex flex-wrap items-center justify-between gap-4 border-b border-gray-200 dark:border-gray-700 pb-4">
 
                             <div class="flex flex-wrap items-center gap-x-6 gap-y-2 text-sm text-gray-500 mt-2"
                                  x-data="{ showEmails_{{ $party->id }}: false }">
@@ -266,8 +276,13 @@
                                 {{-- Phone --}}
                                 @if ($mobilePhone = $party->phones->firstWhere('type', 'MOBILE'))
                                     <span class="flex items-center gap-1.5 min-w-0">
-                                        <svg class="w-5 h-5 text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M18.427 14.768 17.2 13.542a1.733 1.733 0 0 0-2.45 0l-.613.613a1.732 1.732 0 0 1-2.45 0l-1.838-1.84a1.735 1.735 0 0 1 0-2.452l.612-.613a1.735 1.735 0 0 0 0-2.452L9.237 5.572a1.6 1.6 0 0 0-2.45 0c-3.223 3.2-1.702 6.896 1.519 10.117 3.22 3.221 6.914 4.745 10.12 1.535a1.601 1.601 0 0 0 0-2.456Z"/></svg>
-                                        <a href="tel:{{ $mobilePhone->number }}" class="truncate hover:underline" title="{{ $mobilePhone->number }}">{{ $mobilePhone->number }}</a>
+                                        <svg class="w-5 h-5 text-gray-500" aria-hidden="true"
+                                             xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none"
+                                             viewBox="0 0 24 24"><path stroke="currentColor" stroke-linecap="round"
+                                                                       stroke-linejoin="round" stroke-width="2"
+                                                                       d="M18.427 14.768 17.2 13.542a1.733 1.733 0 0 0-2.45 0l-.613.613a1.732 1.732 0 0 1-2.45 0l-1.838-1.84a1.735 1.735 0 0 1 0-2.452l.612-.613a1.735 1.735 0 0 0 0-2.452L9.237 5.572a1.6 1.6 0 0 0-2.45 0c-3.223 3.2-1.702 6.896 1.519 10.117 3.22 3.221 6.914 4.745 10.12 1.535a1.601 1.601 0 0 0 0-2.456Z"/></svg>
+                                        <a href="tel:{{ $mobilePhone->number }}" class="truncate hover:underline"
+                                           title="{{ $mobilePhone->number }}">{{ $mobilePhone->number }}</a>
                                     </span>
                                 @endif
 
@@ -281,10 +296,14 @@
 
                                 @if ($visibleEmail)
                                     <span class="flex items-center gap-1.5 min-w-0 relative">
-                                        <svg class="w-6 h-6 text-gray-800 dark:text-white shrink-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                                            <path stroke="currentColor" stroke-linecap="round" stroke-width="2" d="m3.5 5.5 7.893 6.036a1 1 0 0 0 1.214 0L20.5 5.5M4 19h16a1 1 0 0 0 1-1V6a1 1 0 0 0-1-1H4a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1Z"/>
+                                        <svg class="w-6 h-6 text-gray-800 dark:text-white shrink-0" aria-hidden="true"
+                                             xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none"
+                                             viewBox="0 0 24 24">
+                                            <path stroke="currentColor" stroke-linecap="round" stroke-width="2"
+                                                  d="m3.5 5.5 7.893 6.036a1 1 0 0 0 1.214 0L20.5 5.5M4 19h16a1 1 0 0 0 1-1V6a1 1 0 0 0-1-1H4a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1Z"/>
                                         </svg>
-                                        <a href="mailto:{{ $visibleEmail }}" class="hover:underline truncate" title="{{ $visibleEmail }}">{{ $visibleEmail }}</a>
+                                        <a href="mailto:{{ $visibleEmail }}" class="hover:underline truncate"
+                                           title="{{ $visibleEmail }}">{{ $visibleEmail }}</a>
 
                                         @if ($hiddenCount > 0)
                                             <button type="button"
@@ -304,7 +323,8 @@
                                                  x-cloak
                                             >
                                                 @foreach ($hiddenEmails as $email)
-                                                    <a href="mailto:{{ $email }}" class="hover:underline text-gray-500 dark:text-gray-400 text-sm">{{ $email }}</a>
+                                                    <a href="mailto:{{ $email }}"
+                                                       class="hover:underline text-gray-500 dark:text-gray-400 text-sm">{{ $email }}</a>
                                                 @endforeach
                                             </div>
                                         @endif
@@ -342,7 +362,8 @@
                                         <th scope="col" class="th-input w-[24%]">{{ __('forms.email') }}</th>
                                         <th scope="col" class="th-input w-[10%]">{{ __('forms.status.label') }}</th>
                                         @if($hasAnyActionInTable)
-                                            <th scope="col" class="th-input w-[7%] text-center">{{ __('forms.actions') }}</th>
+                                            <th scope="col"
+                                                class="th-input w-[7%] text-center">{{ __('forms.actions') }}</th>
                                         @endif
                                     </tr>
                                     </thead>
@@ -369,7 +390,8 @@
 
                                             <td class="td-input break-words whitespace-normal align-top">
                                                 @if($positionEmail)
-                                                    <a href="mailto:{{ $positionEmail }}" class="hover:underline" title="{{ $positionEmail }}">{{ $positionEmail }}</a>
+                                                    <a href="mailto:{{ $positionEmail }}" class="hover:underline"
+                                                       title="{{ $positionEmail }}">{{ $positionEmail }}</a>
                                                 @else
                                                     N/A
                                                 @endif
