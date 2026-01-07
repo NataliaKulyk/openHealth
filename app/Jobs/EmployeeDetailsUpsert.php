42da5ceb9cf103073534790c407bcbf8ba8add3d
Vitaliy-1 <vitaliybezsh@gmail.com>
2026-01-07 23:45:57 +0200

    Fix owner employee update

--- app/Jobs/EmployeeDetailsUpsert.php
+++ app/Jobs/EmployeeDetailsUpsert.php
@@ -63,11 +63,11 @@ class EmployeeDetailsUpsert extends EHealthJob
         $validatedData = $response->validate();
 
         Log::info('Processing EmployeeDetailsUpsert for employee:' . $this->employee->id . ', LE:' . ($this->legalEntity->id ?? 'N/A'));
-        $ownerEmployee = Employee::with('party')->where('employee_type', 'OWNER')->where('legal_entity_id', $this->legalEntity->id)->first();
-        // This need for OWNER that has a more than one employee_types for the same party
-        if ($validatedData['party']['uuid'] === $ownerEmployee->party->uuid && !$this->employee?->userId) {
-            $this->employee->userId = $ownerEmployee->userId;
-        }
+//        $ownerEmployee = Employee::with('party')->where('employee_type', 'OWNER')->where('legal_entity_id', $this->legalEntity->id)->first();
+//        // This need for OWNER that has a more than one employee_types for the same party
+//        if ($validatedData['party']['uuid'] === $ownerEmployee->party->uuid && !$this->employee?->userId) {
+//            $this->employee->userId = $ownerEmployee->userId;
+//        }
 
         $this->employee->legalEntityUuid = $this->legalEntity?->uuid;
 
