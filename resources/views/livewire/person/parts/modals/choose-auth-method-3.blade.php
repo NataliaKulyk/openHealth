e1e1e00f759cdc0040a321e696ce88693d905a65
Natalia <nataliakulyk123@gmail.com>
2026-01-08 10:52:54 +0200

    #681 add some modals for person

--- resources/views/livewire/person/parts/modals/choose-auth-method-3.blade.php
+++ resources/views/livewire/person/parts/modals/choose-auth-method-3.blade.php
@@ -20,28 +20,23 @@
                      x-trap.noscroll.inert="showAuthMethodModal"
                      class="modal-content w-full max-w-4xl mx-auto bg-white dark:bg-slate-900 p-6 rounded-lg"
                 >
-                    <legend class="legend mb-8 text-2xl font-bold">{{ __('patients.changing_SMS_method') }}</legend>
+                    <legend class="legend mb-8 text-2xl font-bold">{{ __('Зміна методу автентифікації через СМС') }}</legend>
 
-                    <div class="bg-gray-100 dark:bg-slate-800 rounded-lg p-4 mb-8 flex items-start">
-                        @icon('alert-circle', 'w-5 h-5 text-gray-700 dark:text-gray-300 mr-3 mt-0.5')
-                        <p class="text-sm text-gray-800 dark:text-gray-200">
-                            {{ __('patients.please_clarify_phone_number') }}
-                            <span class="font-bold">+38095123xxxx</span>
+                    <div class="bg-red-100 dark:bg-slate-800 rounded-lg p-4 mb-8 flex items-start">
+                        @icon('alert-circle', 'w-5 h-5 text-red-700 dark:text-red-300 mr-3 mt-0.5')
+                        <p class="text-sm text-red-800 dark:text-red-200">
+                            {{ __('patients.if_patient_not_phone_authentication') }}
                         </p>
                     </div>
 
 
                     <div class="mt-8 flex gap-3">
+                        <button type="button" @click="showAuthMethodModal = false" class="button-minor">
+                            {{ __('forms.back') }}
+                        </button>
+
                         <button type="button" @click="showAuthMethodModal = false" class="button-primary-outline">
-                            {{ __('patients.back_authentication_methods') }}
-                        </button>
-
-                        <button type="button" @click="showAuthMethodModal = false" class="button-primary-outline-red">
-                            {{ __('patients.no_access') }}
-                        </button>
-
-                        <button type="button" @click="showAuthMethodModal = false" class="button-primary">
-                            {{ __('patients.available_access') }}
+                            {{ __('patients.to_authentication_methods') }}
                         </button>
                     </div>
                 </div>
